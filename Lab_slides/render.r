480d2ccc493c7dc1a172b862a33a6764969569b3
Shunkei <kakim002@umn.edu>
2024-09-20 08:15:16 -0500

    update lab3 slides

--- Lab_slides/render.r
+++ Lab_slides/render.r
@@ -1,2 +1,16 @@
 library(here)
-renderthis::to_pdf(here("Lab_slides/lab1/lab1.html"))
+library(renderthis)
+renderthis::to_pdf(here("Lab_slides/lab3/lab3_slides.html"))
+
+to_pdf(
+  "~/Dropbox/UM/TA/apec1101/Gitcontrolled/Lab_slides/lab3/lab3_slides.html""
+)
+
+build_pdf(
+	"~/Dropbox/UM/TA/apec1101/Gitcontrolled/Lab_slides/lab3/lab3_slides.html",
+	complex_slides = TRUE, 
+	partial_slides = TRUE
+	)
+
+
+file.exists("~/Dropbox/UM/TA/apec1101/Gitcontrolled/Lab_slides/lab3/lab3_slides.html")
